9057eeb515a7a56a02c175058d578aaefb3228d3
Elijah Ajayi <femyte@gmail.com>
2021-09-15 23:04:33 +0100

    Created Joke Service - Closes #3

--- spring5-jokes-app-v2/src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
+++ spring5-jokes-app-v2/src/main/java/guru/springframework/spring5jokesappv2/Spring5JokesAppV2Application.java
@@ -1,4 +1,4 @@
-package guru.springframework.sfgpetclinic;
+package guru.springframework.spring5jokesappv2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
